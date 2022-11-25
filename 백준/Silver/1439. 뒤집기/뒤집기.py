ef96849af3539f50d40bd4313d1df235d79f6cdd
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-11-25 10:43:37 +0900

    [Silver V] Title: 뒤집기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -13,9 +13,8 @@ for i in arr_1:
     if "0" in i:
         res_1 += 1
 
-else:
-    for i in arr_0:
-        if "1" in i:
-            res_0 += 1
+for i in arr_0:
+    if "1" in i:
+        res_0 += 1
 
 print(min(res_1, res_0))
