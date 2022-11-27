3e4de768b99d2ef530834e516afa49c96c8399fc
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2022-11-27 15:33:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 31088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,32 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-arr = [str(input()) for _ in range(n)]
 v = [[False for _ in range(n)] for _ in range(n)]
-dn = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-
-
-def bfs(i, j):
-    cnt = 0
-    queue = deque([(i, j)])
-    v[i][j] = True
-    while queue:
-        x, y = queue.popleft()
-        cnt += 1
-        for q, w in dn:
-            dx = x+q
-            dy = y+w
-            if 0 <= dx < n and 0 <= dy < n:
-                if v[dx][dy] == False and arr[dx][dy] == "1":
-                    queue.append((dx, dy))
-                    v[dx][dy] = True
-
-    return cnt
-
-
+arr = [str(input()) for _ in range(n)]
+dn = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+cnt = 0
 res = []
+
+def dfs(i, j):
+    global cnt
+    v[i][j] = True
+    if arr[i][j]=="1":
+        cnt += 1
+    for x, y in dn:
+        dx = i+x
+        dy = j+y
+        if 0 <= dx < n and 0 <= dy < n:
+            if v[dx][dy] == False and arr[i][j] == "1":
+                dfs(dx, dy)
+
 for i in range(n):
     for j in range(n):
         if v[i][j] == False and arr[i][j] == "1":
-            res.append(bfs(i, j))
-
-print(len(res))
+            dfs(i, j)
+            res.append(cnt)
+            cnt = 0
 res.sort()
+print(len(res))
 for i in res:
     print(i)
