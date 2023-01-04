83e2b6baa4ebfee42bb4199b293caebd382900d6
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-01-05 00:05:07 +0900

    [Bronze II] Title: 방 배정, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -1,24 +1,23 @@
-n, k = map(int, input().split())  #학생 수, 한 방 최대 인원 수
-female = [0] * 1000
-male = [0] * 1000
+import sys
+input = sys.stdin.readline
 
-for j in range(n):
-    s, y = map(int, input().split())  #성별, 학년
-    #학년별 인원수 체크
-    if s == 0:  #여자
-        female[y] += 1
-    else:  #남자
-        male[y] += 1
-    
-for j in range(1, 7):
-    if female[j] % k == 0:  #인원수 딱 맞게 떨어질 때
-        female[j] = female[j] // k
+n, k = map(int, input().split())
+arr = [[0, 0] for _ in range(7)]
+
+for i in range(n):
+    s, y = map(int, input().split())
+    arr[y][s] += 1
+
+cnt = 0
+for i in arr:
+    if i[0] % k == 0:
+        cnt += i[0]//k
     else:
-        female[j] = (female[j] // k) + 1
+        cnt += (i[0]//k)+1
 
-    if male[j] % k == 0:
-        male[j] = male[j] // k
+    if i[1] % k == 0:
+        cnt += i[1]//k
     else:
-        male[j] = (male[j] // k) + 1
+        cnt += (i[1]//k)+1
 
-print(sum(female) + sum(male))
+print(cnt)
