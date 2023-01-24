ba6979fab68b27fbbcbf3a66b384c2e16b381213
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-01-24 19:18:55 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 4212 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -0,0 +1,44 @@
+
+def check(hp_arr):
+    global res
+    cnt = 0
+    for hp in hp_arr:
+        if hp <= 0:
+            cnt += 1
+    res = max(res, cnt)
+
+
+def dfs(idx):
+    global eggs, hp_arr, weight_arr, visited
+    if idx == eggs:
+        check(hp_arr)
+        return
+    if hp_arr[idx] > 0:
+        crack_egg = True
+        for i in range(eggs):
+            if hp_arr[i] > 0 and i != idx:
+                crack_egg = False
+                hp_arr[i] -= weight_arr[idx]
+                hp_arr[idx] -= weight_arr[i]
+                dfs(idx+1)
+                hp_arr[i] += weight_arr[idx]
+                hp_arr[idx] += weight_arr[i]
+        if crack_egg:
+            check(hp_arr)
+            return
+    else:
+        dfs(idx+1)
+
+
+eggs = int(input())
+hp_arr = []
+weight_arr = []
+visited = [False for _ in range(eggs)]
+res = 0
+for i in range(eggs):
+    hp, weight = map(int, input().split())
+    hp_arr.append(hp)
+    weight_arr.append(weight)
+
+dfs(0)
+print(res)
