8da0689551b1d277655eac8634206e1c2a5f166f
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-02-19 22:58:19 +0900

    [Bronze I] Title: 색종이, Time: 4024 ms, Memory: 39296 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,28 @@
+def paint(cur_x, cur_y, cur_w, cur_h):
+    cnt = 0
+    for i in range(cur_x, cur_h+cur_x):
+        for j in range(cur_y, cur_w+cur_y):
+            if board[i][j] == 1:
+                continue
+            board[i][j] = 1
+            cnt += 1
+    return cnt
+
+
+n = int(input())
+lst = []
+for _ in range(n):
+    total = 0
+    x, y, w, h = map(int, input().split())
+    lst.append((x, y, w, h))
+
+lst.reverse()
+board = [[0 for _ in range(1001)] for _ in range(1001)]
+res = []
+for x, y, w, h in lst:
+    cnt = paint(y, x, w, h)
+    res.append(cnt)
+
+res.reverse()
+for i in res:
+    print(i)
