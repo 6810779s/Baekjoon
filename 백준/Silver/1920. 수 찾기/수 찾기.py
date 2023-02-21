e0dc4be9c8a4e0646901fb6998c4430fa6fb3d13
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-02-21 23:46:42 +0900

    [Silver IV] Title: 수 찾기, Time: 196 ms, Memory: 51332 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst_A = list(map(int, input().split()))
+m = int(input())
+lst = list(map(int, input().split()))
+dic = {}
+for i in lst_A:
+    dic[i] = 1
+
+for i in lst:
+    if i in dic:
+        print(1)
+    else:
+        print(0)
