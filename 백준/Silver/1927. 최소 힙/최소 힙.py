4902a72e347013e8a228d5f1b239af0f30521bb2
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-02-23 00:57:45 +0900

    [Silver II] Title: 최소 힙, Time: 208 ms, Memory: 118528 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+res = []
+
+for _ in range(n):
+    num = int(input())
+    if num != 0:
+        heapq.heappush(res, num)
+        continue
+    if res:
+        print(heapq.heappop(res))
+    else:
+        print(0)
