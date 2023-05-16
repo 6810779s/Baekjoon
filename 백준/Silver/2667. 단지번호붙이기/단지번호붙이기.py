73e14183a149abea2b84c190cf4f20df8e82c9a0
Eunhee Jang <81551986+6810779s@users.noreply.github.com>
2023-05-16 22:56:02 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,35 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
+
+def bfs(x,y):
+    q = deque([(x,y)])
+    visited[x][y] = True
+    cnt = 1
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx,ny = x+dx[i],y+dy[i]
+            if 0<=nx<n and 0<=ny<n and map_lst[nx][ny]=="1" and not visited[nx][ny]:
+                visited[nx][ny]=True
+                q.append((nx,ny))
+                cnt+=1
+    return cnt
+
+
 
 n = int(input())
-v = [[False for _ in range(n)] for _ in range(n)]
-arr = [str(input()) for _ in range(n)]
-dn = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-cnt = 0
-res = []
-
-def dfs(i, j):
-    global cnt
-    v[i][j] = True
-    if arr[i][j]=="1":
-        cnt += 1
-    for x, y in dn:
-        dx = i+x
-        dy = j+y
-        if 0 <= dx < n and 0 <= dy < n:
-            if v[dx][dy] == False and arr[i][j] == "1":
-                dfs(dx, dy)
-
+map_lst = [list(input()) for _ in range(n)]
+visited=[[False for _ in range(n)] for _ in range(n)]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+home_cnt=0
+res_cnt = []
 for i in range(n):
     for j in range(n):
-        if v[i][j] == False and arr[i][j] == "1":
-            dfs(i, j)
-            res.append(cnt)
-            cnt = 0
-res.sort()
-print(len(res))
-for i in res:
-    print(i)
+        if not visited[i][j] and map_lst[i][j]=="1":
+            res_cnt.append(bfs(i,j))
+            home_cnt+=1
+
+res_cnt.sort()
+print(home_cnt)
+for i in range(home_cnt):
+    print(res_cnt[i])
